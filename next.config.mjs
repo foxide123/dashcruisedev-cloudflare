61627a9b52cbeab24dc3a76671a7eadac4b7377c
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-04-16 02:40:43 +0200

    Cleaning up packages and offloading packages to server instead of workers to reduce size

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,18 @@ const withBundleAnalyzer = (await import('@next/bundle-analyzer')).default({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      "@supabase/supabase-js",
+      "@supabase/ssr",
+      "zod",
+      "next-intl",
+      "lucide-react",
+    ],
+  },
+/*   functions: {
+    mode: "advanced", // enables worker to generate smaller workers
+  }, */
   /* config options here */
   output: 'standalone',
   images: {
@@ -16,6 +28,8 @@ const nextConfig = {
         hostname: "*",
       },
     ],
+    loader: "custom",
+    loaderFile: "./image-loader.ts",
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
