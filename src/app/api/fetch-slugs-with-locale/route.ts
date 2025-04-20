fdb5d204b00541464dfde953a91ed699d8cd2887
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-04-20 03:50:17 +0200

    Improving fetch api

--- src/app/api/fetch-slugs-with-locale/route.ts
+++ src/app/api/fetch-slugs-with-locale/route.ts
@@ -3,21 +3,26 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const revalidate = 60;
 
-export async function GET(request:NextRequest) {
+export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams;
     const postId = searchParams.get("postId");
-    
+
     const supabase = await createClient();
-    const { data, error } = await supabase
-    .from("PostTranslation")
-    .select(`
+
+    let query = supabase.from("PostTranslation").select(`
       slug,
       locale:Locale (
         locale
       )
-    `).eq("post_id", postId);
-  
+    `);
+
+    if (postId) {
+      query = query.eq("post_id", postId);
+    }
+
+    const {data,error} = await query;
+
     if (error) {
       console.error("Fetch error:", error);
       return NextResponse.json(
