68b011053eabf722bdcf0878d6b8bfed72e6407d
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-04-20 03:49:57 +0200

    Improving Blog and BlogPost Pages

---
+++ src/app/[locale]/(main)/blog/[slug]/PostContent.tsx
@@ -0,0 +1,136 @@
+import Image from "next/image";
+import { PostData } from "@/types/blog_types";
+import { Source_Serif_4 } from "next/font/google";
+import DOMPurify from "isomorphic-dompurify";
+
+const source_serif_4 = Source_Serif_4({
+  subsets: ["latin"],
+  weight: ["300", "400", "500"],
+});
+
+export function PostContent({postData}: {postData: PostData}) {
+  return (
+    <div
+      className={`${source_serif_4.className} text-gray-700 lg:w-1/2 font-normal text-medium-body leading-medium-body w-full text-base`}
+    >
+      <h1 className="font-medium text-center text-medium-header leading-medium-header">
+        {postData.introduction}
+      </h1>
+      <div className="mt-10 relative w-full aspect-video">
+        <Image
+          alt="main-image"
+          src="e097d49d-c0e4-4644-f379-8a731753de00/hd1920x1080"
+          fill
+          priority
+          fetchPriority="high"
+          loading="eager"
+        />
+      </div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: DOMPurify.sanitize(postData.body),
+        }}
+        className="mb-50"
+      ></div>
+    </div>
+  );
+}
+
+
+{/*         Section 1
+        <section id="what-is-seo-and-why-it-matters">
+          <h2 className="mt-10 text-center leading-medium-section text-medium-section">
+            🧠 What Is SEO and Why It Matters
+          </h2>
+          <p className="my-5">
+            <b>Search Engine Optimization (SEO)</b> is the process of the
+            website optimization that aims to make the website rank higher in
+            search results. <br />
+            <br />
+            <span className="bg-green-50">
+              By finding the right keywords, optimizing website performance as
+              well as other metrics, we can increase the chances of the website
+              being noticed.{" "}
+            </span>
+            <br />
+            If someone is searching for example for a guitar to buy online, the
+            google will first recommend websites that are most optimized in
+            terms of SEO for the search (guitars in this case). Most of the time
+            these websites would be fast, have plenty of pages with quality
+            content and would be trusted by customers. <br />
+            <br />
+            The higher the website is positioned in Google, the higher is the
+            change of a user visiting our website.
+            <br />
+            In this guide we will take a look at some of the methods to improve
+            our website&apos;s ranking in Google.
+          </p>
+        </section>
+        Section 2
+        <section id="how-search-engines-work">
+          <h2 className="text-center leading-medium-section text-medium-section">
+            🕵️‍♀️ How Search Engines Work
+          </h2>
+          <p className="my-5">
+            <span className="text-gray-600">
+              Image idea: A cartoon magnifying glass over a search bar with
+              results listed below.
+            </span>{" "}
+            <br /> Search engines like Google use bots to crawl web pages and
+            index their content. Then, they use algorithms to decide which pages
+            best match a search query. SEO helps your content match those
+            criteria.
+          </p>
+        </section>
+
+        Section 3
+        <section id="3-key-elements-of-seo">
+          <h2 className="text-center leading-medium-section text-medium-section">
+            🔑 3 Key Elements of SEO
+          </h2>
+          <ol className="list-decimal pl-5 my-5">
+            <li>
+              {" "}
+              On-Page SEO Image idea: A web page with highlighted keywords and a
+              well-structured heading layout. This includes using the right
+              keywords, optimizing headings, writing meta descriptions, and
+              creating helpful, readable content
+            </li>
+            <li>
+              Technical SEO Image idea: Gears behind a website, representing
+              backend optimizations. This involves improving website speed,
+              making sure your site is mobile-friendly, and having a proper site
+              structure for bots to crawl
+            </li>
+            <li>
+              Off-Page SEO Image idea: Websites linking to your site, shown as
+              connected circles. This is mostly about backlinks—other websites
+              linking to your content. The more trustworthy those sites are, the
+              more valuable their link
+            </li>
+          </ol>
+        </section>
+
+        Section 4
+        <section id="why-you-should-care">
+          <h2 className="text-center leading-medium-section text-medium-section">
+            🚀 Why You Should Care
+          </h2>
+          <p className="my-5">
+            {" "}
+            More Visibility = more traffic. SEO builds trust—users trust
+            top-ranked results. It gives you a long-term advantage without
+            paying for ads.
+          </p>
+        </section>
+
+        Section 5 
+        <section id="quick-tips-for-beginners">
+          <h2 className="text-center leading-medium-section text-medium-section">
+            📈 Quick Tips for Beginners
+          </h2>
+          <p className="my-5">
+            Use tools like Google Search Console and Yoast SEO. Write for humans
+            first, search engines second. Focus on quality over quantity.
+          </p>
+        </section> */}
--- src/app/[locale]/(main)/blog/[slug]/page.tsx
+++ src/app/[locale]/(main)/blog/[slug]/page.tsx
@@ -1,52 +1,124 @@
 import { routing } from "@/i18n/routing";
 /* import { createClient } from "@/utils/supabase/client"; */
 import { setRequestLocale } from "next-intl/server";
+import { SocialIcons2 } from "@/components/common/SocialIcons2";
+import { ArticleSidebarNav } from "@/components/layout/ArticleSidebarNav";
+import { PostContent } from "./PostContent";
+import { PostData } from "@/types/blog_types";
 import { notFound } from "next/navigation";
+/* import { PostParams } from "@/types/main_types";
+ */
 
 export const dynamicParams = false;
 export const dynamic = "force-static";
 //revalidate every 10 minutes
 export const revalidate = 600;
 
-const blogSlugs = [
-  { slug: "seo-introduction" },
-  { slug: "website-builders" },
-  { slug: "professional-email" },
-  { slug: "add-business-on-google-maps" },
-];
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
-export async function generateMetadata() {
+type SlugWithLocale = {
+  slug: string;
+  locale: string;
+};
+
+function generateHreflangs(slugsWithLocale: SlugWithLocale[]) {
+  //eslint-disable-next-line
+  const languages: any = {};
+  console.log("slugs with locale: ");
+
+  slugsWithLocale.forEach((item: SlugWithLocale) => {
+    const { slug, locale } = item;
+    languages[locale] = `${baseUrl}/${locale}/${slug}`;
+  });
+
+  return { languages: languages };
+}
+
+async function fetchSlugsWithLocale(postId?: string) {
+  try {
+    let query = `${baseUrl}/api/fetch-slugs-with-locale`;
+    if (postId) {
+      query = `${query}?postId=${postId}`;
+    }
+
+    const response = await fetch(query);
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error("Error response:", errorText);
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
+    }
+
+    const resultJson = await response.json();
+
+    if (resultJson["error"]) {
+      console.error(resultJson["error"]);
+    }
+    const result = resultJson["data"];
+
+    //eslint-disable-next-line
+    const slugsWithLocale = result.map((item: any) => {
+      return { slug: item["slug"], locale: item["locale"]["locale"] };
+    });
+    console.log("Slugs with locale:", slugsWithLocale);
+
+    return slugsWithLocale;
+  } catch (error) {
+    console.error(
+      "Error fetching slugs:",
+      error instanceof Error ? error.message : error
+    );
+    return null;
+  }
+}
+
+async function fetchPostBySlug(slug: string): Promise<PostData | null> {
+  try {
+    const response = await fetch(
+      `${baseUrl}/api/fetch-post-by-slug?slug=${slug}`
+    );
+    if (!response.ok) {
+      const text = await response.text();
+      throw new Error(text);
+    }
+    const jsonResponse = await response.json();
+    return jsonResponse["data"][0];
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string; slug: string }>;
+}) {
+  const { locale, slug } = await params;
+  const post: PostData | null = await fetchPostBySlug(slug);
+  if (!post || post === null) notFound();
+
+  const slugsWithLocale = await fetchSlugsWithLocale(post!.post_id);
+  if (!slugsWithLocale) notFound();
+
+  const { languages } = generateHreflangs(slugsWithLocale);
+  console.log("languages:", languages);
+  const canonical = `${baseUrl}/${locale}/${slug}`;
   return {
     title: "DashcruiseDev - Blog",
     description: "Modern Tech in Business: Insights & Strategies",
     alternates: {
-      canonical: "https://dashcruisedev.com/en/blog",
-      languages: {
-        en: "https://dashcruisedev.com/en/blog",
-        de: "https://dashcruisedev.com/de/blog",
-        pl: "https://dashcruisedev.com/pl/blog",
-        ro: "https://dashcruisedev.com/ro/blog",
-      },
+      canonical: canonical,
+      languages: languages,
     },
   };
 }
 
 export async function generateStaticParams() {
-  /*  const supabase = createClient();
-  const { data: slugs, error } = await supabase.from("PostTranslation").select("slug");
- */
-  /* Combine locale with slug e.g. {locale: 'en', slug: '...'} 
-  }*/
-
-  /* const messages = await getMessages();
-  const blogSlugs = messages.posts.map((post: PostParams) => ({
-    slug: post.slug as string,
-  })); */
-
-  const params = routing.locales.flatMap((locale) =>
-    blogSlugs?.map(({ slug }: { slug: string }) => ({ locale, slug }))
-  );
-  return params;
+  const slugsWithLocale = await fetchSlugsWithLocale();
+  if(slugsWithLocale === null) notFound();
+  return slugsWithLocale;
 }
 
 export default async function BlogPost({
@@ -62,11 +134,42 @@ export default async function BlogPost({
   }
 
   setRequestLocale(locale);
-/*   const res = await fetch("/api/fetch-post-by-slug", {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ slug }),
-  });
-  console.log(res); */
-  return <div>This is the blog page for post {slug}</div>;
+  //eslint-disable-next-line
+  const postData: PostData | null = await fetchPostBySlug(slug);
+  if (!postData || postData === null) notFound();
+
+  const lg_screen_width = "lg:w-[75vw]";
+  const default_screen_width = "w-[85vw]";
+
+  return (
+    <div
+      className={`caret-transparent ${lg_screen_width} ${default_screen_width} mx-auto min-h-screen flex md:flex-row flex-col justify-center`}
+    >
+      {/* Sidebar navigation */}
+      <ArticleSidebarNav />
+
+      {/* Main Content in the Middle */}
+      <PostContent postData={postData!} />
+
+      {/* Social Media On the Right*/}
+      <div className="md:sticky md:block hidden w-[200px] h-[250px] shadow-md ml-20 py-5 rounded-sm top-20 left-0 mt-50 flex flex-col justify-center">
+        <h2 className="text-center mb-5 w-1/2 mx-auto">
+          Check Our Social Media
+        </h2>
+        <SocialIcons2 linkedin_url="s" fb_url="s" direction="flex-col" />
+      </div>
+      {/* Social Media on Small Screens */}
+      <div className="md:hidden block shadow-md py-5 my-5 rounded-sm w-full flex-wrap">
+        <h2 className="text-center mb-5 w-1/2 mx-auto">
+          Check Our Social Media
+        </h2>
+        <SocialIcons2
+          linkedin_url="https://www.linkedin.com/in/jakub-cezary-kolando-137491269/"
+          fb_url="https://www.facebook.com/profile.php?id=61574463432787&notif_id=1743636704425943&notif_t=page_user_activity&ref=notif"
+          ig_url="https://www.instagram.com/perspective_135/"
+          direction="flex-row"
+        />
+      </div>
+    </div>
+  );
 }
