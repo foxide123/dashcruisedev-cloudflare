ad1bee9a1d2522403dd2eaa0dc7e66ca617ea36f
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-04-22 13:39:14 +0200

    Going back to kv Incremental cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,11 +1,7 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 
 export default defineCloudflareConfig({
-  incrementalCache: withRegionalCache(r2IncrementalCache, {
-    mode: "long-lived",
-    shouldLazilyUpdateOnCacheHit: true,
-  }),
+  incrementalCache: kvIncrementalCache,
 });
