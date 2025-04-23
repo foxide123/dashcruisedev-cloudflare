75c0d06a4357c3c8a272f25ccc13317c60e8bc95
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-04-23 13:34:13 +0200

    revert c0ebd849b9a452996bf4e5d82bad2e3ad098e956

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 import createNextIntlPlugin from 'next-intl/plugin';
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-
-initOpenNextCloudflareForDev();
 
 const withNextIntl = createNextIntlPlugin();
 
