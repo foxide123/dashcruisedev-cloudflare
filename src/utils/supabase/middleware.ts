535da6ab0d9cc3a655b3da9672eddea659b9cbb0
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-06-23 13:39:04 +0200

    updating middleware

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,26 +1,25 @@
-import { createServerClient } from "@supabase/ssr";
-import { NextResponse, type NextRequest } from "next/server";
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
 
-export async function updateSession(
-  request: NextRequest,
-  response: NextResponse
-) {
+export async function updateSession(request: NextRequest, response:NextResponse) {
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll: () => request.cookies.getAll(),
-        setAll: (cookiesToSet) => {
+        getAll() {
+          return request.cookies.getAll()
+        },
+        setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value, options }) =>
             response.cookies.set(name, value, options)
-          );
+          )
         },
       },
     }
-  );
+  )
 
-  const {
+ const {
     data: { session },
   } = await supabase.auth.getSession();
 
